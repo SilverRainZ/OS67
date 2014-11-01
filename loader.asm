03b3a94a977ea97a28abd385eb90591d5e7c39c9
LastAvenger <LastAvengers@outlook.com>
2014-11-01 23:27:19 +0800

    Add stack segment.
    强迫症发作, 大写还是小写?!

--- loader.asm
+++ loader.asm
@@ -6,127 +6,147 @@
 %INCLUDE "pm.inc"
 
 [BITS 16]
-ORG 0x8000
-JMP loader
+org 0x8000
+jmp loader
 
 msg_loader:
-    DB "loader loaded."
-    DB 13,10
-    DB 0
+    db "loader loaded."
+    db 13,10
+    db 0
 
 temp_print16:
 _loop:
-    LODSB
-    OR AL,AL
+    lodsb
+    OR al,al
 	JZ _done 
-	MOV	AH,0x0e		
-	MOV	BX,15		
-	INT	0x10		
-	JMP	_loop
+	mov	ah,0x0e		
+	mov	bx,15		
+	int	0x10		
+	jmp	_loop
 _done:
     RET
 
 loader:
-    XOR ESI, ESI
-    MOV SI,msg_loader
-    CALL temp_print16
-    JMP LABEL_BEGIN
+    xor esi, esi
+    mov si,msg_loader
+    call temp_print16
+    jmp BEGIN
 
 ; ==================================================================
 [SECTION .gdt]
-    LABEL_GDT:	        Descriptor 0,       0,                  0           ; null
-    LABEL_DESC_CODE32:  Descriptor 0,       SegCode32Len - 1,   DA_C + DA_32; 非一致代码段
-    LABEL_DESC_DATA0:   Descriptor 0,       SegData0Len - 1,    DA_DRW      ; data seg r0
-    LABEL_DESC_VIDEO:   Descriptor 0B8000h, 0ffffh,             DA_DRW      ; video RAM
+GDT:
+DESC_NULL:        Descriptor 0,       0,                  0             ; null
+DESC_CODE32_R0:   Descriptor 0,       SegCode32Len - 1,   DA_C+DA_32    ; 非一致代码段
+DESC_CODE16_R0:   Descriptor 0,       0xffff,             DA_C		    ; 非一致,16
+DESC_DATA_R0:     Descriptor 0,       SegData0Len - 1,    DA_DRW        ; data seg r0
+DESC_VIDEO_R0:    Descriptor 0xb8000, 0xffff,             DA_DRW        ; video RAM
+DESC_STACK_R0:    Descriptor 0,       TopOfStack_R0,      DA_DRWA+DA_32
+DESC_STACK_R3:    Descriptor 0,       TopOfStack_R3,      DA_DRWA+DA_32+DA_DPL3
 
-    GdtLen	EQU	$ - LABEL_GDT	; GDT len
-    GdtPtr	DW	GdtLen - 1	    ; GDT limit
-    DD	0		                ; GDT Base
+GdtLen	equ	$ - GDT	; GDT len
+GdtPtr	DW	GdtLen - 1	    ; GDT limit
+DD	0		                ; GDT Base
 
 ; GDT Selector 
-    SelectorCode32	EQU	LABEL_DESC_CODE32 - LABEL_GDT
-    SelectorData0   EQU LABEL_DESC_DATA0 - LABEL_GDT
-    SelectorVideo	EQU	LABEL_DESC_VIDEO - LABEL_GDT
+Selec_Code32_R0	equ	DESC_CODE32_R0    - DESC_NULL
+Selec_Data_R0   equ DESC_DATA_R0      - DESC_NULL 
+Selec_Video_R0	equ	DESC_VIDEO_R0     - DESC_NULL
+Selec_Stack_R0  equ	DESC_STACK_R0     - DESC_NULL
+Selec_Stack_R3  equ	DESC_STACK_R3     - DESC_NULL
 ; END of [SECTION .gdt]
 
 [SECTION .s16]
-LABEL_BEGIN:
-    MOV	AX, CS
-    MOV	DS, AX
-    MOV	ES, AX
-    MOV	SS, AX
-    MOV	SP, 0x0100
+BEGIN:
+    mov	ax, CS
+    mov	DS, ax
+    mov	ES, ax
+    mov	SS, ax
+    mov	SP, 0x7c00  ; temp 
 
-    FillDesc CS, LABEL_SEG_CODE32, LABEL_DESC_CODE32
-    FillDesc DS, LABEL_SEG_DATA0, LABEL_DESC_DATA0
+    FillDesc CS, SEG_CODE32_R0, DESC_CODE32_R0
+    FillDesc DS, SEG_DATA_R0, DESC_DATA_R0
+    FillDesc DS, SEG_STACK_R0, DESC_STACK_R0
+    FillDesc DS, SEG_STACK_R3, DESC_STACK_R3
 
     ; 为加载 GDTR 作准备
-    XOR	EAX, EAX
-    MOV	AX, DS
-    SHL	EAX, 4
-    ADD	EAX, LABEL_GDT		; EAX <- gdt 基地址
-    MOV	dword [GdtPtr + 2], EAX	; [GdtPtr + 2] <- gdt 基地址
+    xor	eax, eax
+    mov	ax, DS
+    SHL	eax, 4
+    add	eax, GDT		; eax <- gdt 基地址
+    mov	dword [GdtPtr + 2], eax	; [GdtPtr + 2] <- gdt 基地址
 
     LGDT	[GdtPtr]
     CLI
 
-    IN	AL, 0x92    ; turn on A20 line
-    OR	AL, 00000010b
-    OUT	0x92, AL
+    IN	al, 0x92    ; turn on A20 line
+    OR	al, 00000010b
+    OUT	0x92, al
 
     ; 准备切换到保护模式
-    MOV	EAX, CR0
-    OR	EAX, 1
-    MOV	CR0, EAX
+    mov	eax, cr0
+    OR	eax, 1
+    mov	cr0, eax
 
-    JMP	dword SelectorCode32:0	; special, clear pipe-line and jump 
+    jmp	dword Selec_Code32_R0:0	; special, clear pipe-line and jump 
 ; END of [SECTION .s16]
  
 [SECTION .s32]; 32 位代码段. 由实模式跳入.
 [BITS 32]
 
-LABEL_SEG_CODE32:
-    MOV AX, SelectorData0
-    MOV DS, AX
+SEG_CODE32_R0:
+    mov ax, Selec_Data_R0
+    mov ds, ax
+    mov ax, Selec_Stack_R0
+    mov	ss, ax
+    mov esp, TopOfStack_R0
 
-    MOV	AX, SelectorVideo
-    MOV	GS, AX	 
-    MOV	EDI,(160 * 2) + 0  	; 160 * 50  line 3 column 1 
-    MOV	AH, 00001011b	; 0000: black 1100: cyan , i love this beautiful word
+    mov	ax, Selec_Video_R0 
+    mov	gs, ax	 
+    mov	edi,(160 * 2) + 0  	; 160 * 50  line 3 column 1 
+    mov	ah, 00001011b	; 0000: black 1100: cyan , i love this beautiful word
 
-    XOR ESI,ESI         ; be careful !
-    MOV SI, msg_GDT
-    CALL temp_print32 
+    xor esi,esi         ; be careful !
+    mov si, msg_GDT
+    call temp_print32 
 
-    JMP $
+    jmp $
 
 temp_print32:
-    ADD EDI, 160
-    PUSH EDI
-    CLD
+    add edi, 160
+    push edi
+    cld
 loop:
-    LODSB
-    ;MOV AL,[SI]
-    ;ADD SI,1
-    CMP AL, 0
-    JE outloop
-    MOV	[GS:EDI], AX
-    ADD EDI,2
-    JMP loop
+    lodsb
+    ;mov al,[si]
+    ;add si,1
+    cmp al, 0
+    je outloop
+    mov	[gs:edi], ax
+    add edi,2
+    jmp loop
 
 outloop:
-    POP EDI
-    RET
+    pop edi
+    ret
 
-SegCode32Len    EQU	$ - LABEL_SEG_CODE32
+SegCode32Len    equ	$ - SEG_CODE32_R0
 
 [SECTION .data0]
-LABEL_SEG_DATA0:
+SEG_DATA_R0:
 
 _msg_GDT:
-    DB "GDT loaded."
-    DB  0
-msg_GDT EQU _msg_GDT - $$
+    db "GDT loaded."
+    db  0
+msg_GDT equ _msg_GDT - $$
 
-SegData0Len  EQU $ - LABEL_SEG_DATA0
+SegData0Len  equ $ - SEG_DATA_R0
+
+[SECTION .stack]
+SEG_STACK_R0:   ; ring 0 stack segment 
+    times 512 db 0
+TopOfStack_R0 equ $ - SEG_STACK_R0 - 1
+
+SEG_STACK_R3: ; ring 3 stack segment
+	times 512 db 0
+TopOfStack_R3 equ $ - SEG_STACK_R3 - 1
 
