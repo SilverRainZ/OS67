0c636256c29bb6bac2a82841a685206fd41c82ae
lastavengers <lastavengers@outlook.com>
2015-09-12 21:11:36 +0800

    modified pipe.c

--- inc/pipe.h
+++ inc/pipe.h
@@ -15,7 +15,7 @@ struct pipe {
 };
 
 int pipe_alloc(struct file **f0, struct file **f1);
-int pipe_close(struct pipe *p, int writeable);
+void pipe_close(struct pipe *p, int writeable);
 int pipe_write(struct pipe *p, char *addr, int n);
 int pipe_read(struct pipe *p, char *addr, int n);
 
--- kern/pipe.c
+++ kern/pipe.c
@@ -9,6 +9,8 @@
 #include <file.h>
 // pipe
 #include <pipe.h>
+// proc
+#include <proc.h>
 
 int pipe_alloc(struct file **f0, struct file **f1){
     struct pipe *p;
@@ -35,15 +37,55 @@ int pipe_alloc(struct file **f0, struct file **f1){
 
     return 0;
 }
-int pipe_close(struct pipe *p, int writeable){
 
+void pipe_close(struct pipe *p, int writeable){
+    if (writeable){
+        p->writeopen = 0;
+        wakeup(&p->nread);
+    } else {
+        p->readopen= 0;
+        wakeup(&p->nwrite);
+    }
+
+    if (p->readopen == 0 && p->writeopen == 0){
+        pmm_free((uint32_t)p);
+    }
 }
 
 int pipe_write(struct pipe *p, char *addr, int n){
+    int i; 
+    for (i = 0; i < n; i++){
+        while (p->nwrite == p->nread + PIPE_SIZE){   // pipe is full
+            if (p->readopen == 0 || proc->killed){
+                return -1;
+            }
+            wakeup(&p->nread);  // wakeup pipe_read
+            sleep(&p->nwrite);
+        }
+        p->data[p->nwrite++ % PIPE_SIZE] = addr[i];
+    }
 
+    wakeup(&p->nread);
+    return n;
 }
 
 int pipe_read(struct pipe *p, char *addr, int n){
+    int i;
 
+    while (p->nread == p->nwrite && p->writeopen){  // pile is empty and writeable
+        if (proc->killed){
+            return -1;
+        }
+        sleep(&p->nread);
+    }
+    for (i = 0; i < n; i++){
+        if (p->nread == p->nwrite){  // empty
+            break;
+        }
+
+        addr[i] = p->data[p->nread++ % PIPE_SIZE];
+    }
+    wakeup(&p->nwrite);
+
+    return i;
 }
-
