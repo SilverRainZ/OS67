0561f48818a8479b7b960ffc119f06a1d5352d74
lastavengers <lastavengers@outlook.com>
2015-10-07 16:43:27 +0800

    keyboard driver(drv/kb.c) understand shift ctrl and  alt now borrow code form fleurix, xv6's kbd.c is hard to read without comments

--- drv/kb.c
+++ drv/kb.c
@@ -1,6 +1,11 @@
+#define __LOG_ON 1
+
 /* kb.c 
  * This file is modified form Bram's Kernel Development Tutorial
  * enable keyboard and retive message by IRQ
+ * October is bad :(
+ * http://bbs.chinaunix.net/thread-1999379-1-1.html
+ * borrow code form: https://github.com/Fleurer/fleurix/blob/master/src/chr/keybd.c
  */
 // std
 #include <type.h>
@@ -10,118 +15,183 @@
 // drv
 #include <kb.h>
 #include <vga.h>
+// libs
+#include <printk.h>
+// fs
+#include <file.h>
+// pipe
+#include <pipe.h>
 
-static unsigned char kb_map[128] =
-{
+static uint32_t kb_mode = 0;
+struct file *kb_bufin, *kb_bufout;
+
+/* maybe scancode set 1 ?*/
+static char kb_map[128] = {
     0,  
-    27, /* esc */
+    0x1b,   /* esc */
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
     '-', '=', '\b', '\t', 'q', 'w', 'e', 'r',    
     't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
-    0,  /* Left Control */
+    0,      /* left ctrl */
     'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';','\'', '`',
-    0,    /* Left shift */
+    0,      /* left shift */
     '\\', 'z', 'x', 'c', 'v', 'b', 'n',
     'm', ',', '.', '/',   
-    0,    /* Right shift */
+    0,    /* right shift */
     '*',
-    0,  /* Alt */
-    ' ',/* Space bar */
-    0,  /* Caps lock */
-    0,    /* 59 - F1 key ... > */
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0,    /* < ... F10 */
-    0,    /* 69 - Num lock*/
-    0,    /* Scroll Lock */
-    0,    /* Home key */
-    0,    /* Up Arrow */
-    0,    /* Page Up */
+    0,  /* alt */
+    ' ',/* space*/
+    0,  /* capslock */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   /* f1 ... f10 */
+    0,    /* num lock*/
+    0,    /* scroll Lock */
+    0,    /* home key */
+    0,    /* up arrow */
+    0,    /* page up */
     '-',
-    0,    /* Left Arrow */
+    0,    /* left arrow */
     0,
-    0,    /* Right Arrow */
+    0,    /* right arrow */
     '+',
-    0,    /* End key*/
-    0,    /* Down Arrow */
-    0,    /* Page Down */
-    0,    /* Insert Key */
-    0,    /* Delete Key */
+    0,    /* end key*/
+    0,    /* down arrow */
+    0,    /* page down */
+    0,    /* insert key */
+    0,    /* delete key */
     0,   0,   0,
-    0,    /* F11 Key */
-    0,    /* F12 Key */
-    0,    /* All other keys are undefined */
+    0,    /* f11 key */
+    0,    /* f12 key */
+    0,    /* all other keys are undefined */
 };
 
-static unsigned char kb_map_shift[128] =
+static char kb_shift_map[128] =
 {
     0,  
-    27, /* esc */
+    0x1b, /* esc */
     '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
     '_', '+', '\b', '\t', 'Q', 'W', 'E', 'R',    
     'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',
-    0,  /* Left Control */
+    0,  /* left control */
     'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':','\"', '~',
-    0,    /* Left shift */
+    0,    /* left shift */
     '|', 'Z', 'X', 'C', 'V', 'B', 'N',
     'M', '<', '>', '?',   
-    0,    /* Right shift */
+    0,    /* right shift */
     '*',
-    0,  /* Alt */
-    ' ',/* Space bar */
-    0,  /* Caps lock */
-    0,    /* 59 - F1 key ... > */
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0,    /* < ... F10 */
-    0,    /* 69 - Num lock*/
-    0,    /* Scroll Lock */
-    0,    /* Home key */
-    0,    /* Up Arrow */
-    0,    /* Page Up */
+    0,  /* alt */
+    ' ',/* space bar */
+    0,  /* caps lock */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   /* f1 ... f10 */
+    0,    /* num lock*/
+    0,    /* scroll lock */
+    0,    /* home key */
+    0,    /* up arrow */
+    0,    /* page up */
     '-',
-    0,    /* Left Arrow */
+    0,    /* left arrow */
     0,
-    0,    /* Right Arrow */
+    0,    /* right arrow */
     '+',
-    0,    /* End key*/
-    0,    /* Down Arrow */
-    0,    /* Page Down */
-    0,    /* Insert Key */
-    0,    /* Delete Key */
+    0,    /* end key*/
+    0,    /* down arrow */
+    0,    /* page down */
+    0,    /* insert key */
+    0,    /* delete key */
     0, 0, 0,
-    0,    /* F11 Key */
-    0,    /* F12 Key */
-    0,    /* All other keys are undefined */
+    0,    /* f11 key */
+    0,    /* f12 key */
+    0,    /* all other keys are undefined */
 };
 
-void kb_handler(struct int_frame *r){
-    unsigned char scancode;
-    kb_map_shift[0] = 0;
-    /* Read from the keyboard's data buffer */
-    scancode = inb(0x60);
-    /* If the top bit of the byte we read from the keyboard is
-     *  set, that means that a key has just been released */
-    if (scancode & 0x80){
-        /* You can use this one to see if the user released the
-         *  shift, alt, or control keys... */
-    }
-    else
-    {
-        /* Here, a key was just pressed. Please note that if you
-         *  hold a key down, you will get repeated key press
-         *  interrupts. */
+static char shift(char sc){
+    char ch = sc & 0x7f;    // clear highest bit
 
-        /* Just to show you how this works, we simply translate
-         *  the keyboard scancode into an ASCII value, and then
-         *  display it to the screen. You can get creative and
-         *  use some flags to see if a shift is pressed and use a
-         *  different layout, or you can add another 128 entries
-         *  to the above layout to correspond to 'shift' being
-         *  held. If shift is held using the larger lookup table,
-         *  you would add 128 to the scancode when you look for it */
-        putchar(kb_map[scancode]);
+    /* the previous scancode is 0xe0*/
+    if (kb_mode & E0ESC) { 
+        switch (ch) {
+            case 0x1D: return CTRL;
+            case 0x38: return ALT;
+        }
+    }
+    else {
+        switch(ch) {
+            case 0x2A:
+            case 0x36: return SHIFT;
+            case 0x1D: return CTRL;
+            case 0x38: return ALT;
+        }
+    }
+    return 0;
+}
+
+void kb_handler(struct int_frame *r){
+    uint8_t sc, m;
+    char ch;
+
+    if ((inb(KB_STAT) & KB_STAT_OBF) == 0){
+        /* no data to read */
+        return;
+    }
+
+    /* read */
+    sc = inb(KB_DATA);
+    printl("kb_handler: sc: 0x%x\n", sc);
+
+    /* is a escape char? */
+    if (KB_IS_ESCAPE(sc)){
+        printl("kb_handler: ESCAPT\n");
+        kb_mode |= E0ESC;
+        return;
+    } 
+    
+    /* check alt shift and ctrl */
+    if ((m = shift(sc))){
+        if (KB_IS_RELEASE(sc)){
+            /* clear mode when release the key */
+            kb_mode &= ~m;
+        } else {
+            kb_mode |= m;
+        }
+        return;
+    }
+
+    if (kb_mode & SHIFT){
+        printl("kb_handler: SHIFT\n");
+
+        ch = kb_shift_map[sc & 0x7f];
+    } else {
+        ch = kb_map[sc & 0x7f];
+    }
+
+    if (kb_mode & CTRL){
+        printl("kb_handler: CTRL\n");
+        switch(ch){
+            // TODO
+            // console control char
+        }
+    }
+
+    if (kb_mode & ALT){
+        printl("kb_handler: ALT\n");
+        /* nothing to do ? */
+    }
+
+    /* on release */
+    if (KB_IS_RELEASE(sc)){
+        printl("kb_handler: release\n");
+    } 
+    /* on press */
+    else if (ch != 0) {
+        printl("kb_handler: char: [%c]\n", ch);
+
+        /* write to kernel input buffer */
+        if (pipe_push(kb_bufin->pipe, ch) < 0){
+            printl("kb: buffer overflow.\n");
+        }
     }
 }
 
 void kb_init(){
     irq_install(IRQ_KB, kb_handler);
+    pipe_alloc(&kb_bufout, &kb_bufin);
 }
--- inc/kb.h
+++ inc/kb.h
@@ -1,4 +1,21 @@
 #ifndef __KB_H
 #define __KB_H
+
+#define KB_DATA     0x60
+#define KB_STAT     0x64
+#define KB_CMD      0x64
+
+/* output buffer full */
+#define KB_STAT_OBF     0x01
+
+#define SHIFT       (1<<0)
+#define CTRL        (1<<1)
+#define ALT         (1<<2)
+
+#define E0ESC       (1<<6)
+
+#define KB_IS_RELEASE(sc)   (sc & 0x80) 
+#define KB_IS_ESCAPE(sc)    (sc == 0xe0)
+
 void kb_init();
 #endif
