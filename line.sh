304b7116bfb2993b94fd2dbb4639e4e9faf87405
lastavengers <lastavengers@outlook.com>
2015-10-17 02:11:07 +0800

    add ulink.ld: section alignment = 0x1000 remove unused comments in line.sh

--- line.sh
+++ line.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
-cat /dev/null > info          #清空info
-types=("Makefile" "*.asm" "*.c" "*.h" )    #所有要统计的文件类型
-for i in ${types[@]}          #遍历每个文件类型
+cat /dev/null > info
+types=("Makefile" "*.asm" "*.c" "*.h")
+for i in ${types[@]}
 do
-    find . -iname $i > tmp    #按类型（大小写不敏感）查找，缓存到tmp
-    cat tmp >> info           #将查询结果追加到info
+    find . -iname $i > tmp
+    cat tmp >> info
 done
-sed -i 's/ /\\&/g' info       #处理文件名出现空格的情况
-cat info | xargs wc -l        #统计行数
-rm info tmp                   #删除临时文件
+sed -i 's/ /\\&/g' info
+cat info | xargs wc -l
+rm info tmp
