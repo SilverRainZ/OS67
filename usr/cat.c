bd99f2209df6b46362cb5e43f079fb75ffa47d5b
lastavengers <lastavengers@outlook.com>
2016-01-26 00:18:04 +0800

    <(=﹁"﹁=)> kitten grows up~ #7 * cat can concatenate mulit files now * modify usr/README

--- usr/cat.c
+++ usr/cat.c
@@ -6,41 +6,58 @@
 
 char buf[CAT_BUF];
 
-int main(int argc, char **argv){
+/* Arthur2e5: Meow~ */
+void kitten(){
     int n;
-    int fd = -1;
+
+    for (;;){
+        n = _read(stdin, buf, sizeof(buf) - 1);
+        if (n <= 0){
+            break;
+        }
+        buf[n] = '\0';
+        puts(buf);
+    }
+}
+
+int main(int argc, char **argv){
+    int i, fd = -1;
     struct stat st;
 
     if (argc > 1){
-        _close(0);
-        if ((fd = _open(argv[1], O_RONLY)) < 0){
-            printf("cat: can not open `%s`\n", argv[1]);
-            goto exit;
-        }
+        /* read from files */
+        i = 1;
+        while (i < argc){
+            _close(0);
+            if ((fd = _open(argv[i], O_RONLY)) < 0){
+                printf("cat: can not open `%s`\n", argv[i]);
+                goto exit;
+            }
 
-        if (fd != stdin){
-            printf("cat: wrong file descriptor %d", fd);
-            goto exit;
-        }
+            if (fd != stdin){
+                printf("cat: wrong file descriptor %d", fd);
+                goto exit;
+            }
 
-        if (_fstat(fd, &st) < 0){
-            printf("cat: can not get stat of `%s`\n", argv[1]);
-            goto exit;
-        }
+            if (_fstat(fd, &st) < 0){
+                printf("cat: can not get stat of `%s`\n", argv[i]);
+                goto exit;
+            }
 
-        if (!S_ISREG(st.mode)){
-            printf("cat: `%s` is not a file\n", argv[1]);
-            goto exit;
-        }
-    }
+            if (!S_ISREG(st.mode)){
+                printf("cat: `%s` is not a file\n", argv[i]);
+                goto exit;
+            }
 
-    for (;;){
-        n = _read(stdin, buf, sizeof(buf));
-        buf[n] = '\0';
-        puts(buf);
-        if (n == 0){
-            break;
+            kitten();
+
+            _close(fd);
+            fd = -1;
+            i++;
         }
+    } else {
+        /* read form stdin */
+        kitten();
     }
 
 exit:
